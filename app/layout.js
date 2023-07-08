9a681b12c084e9f77749e4310a4a43aa1e299111
Justin Sun <justinsunyt@gmail.com>
2023-07-08 18:23:20 +0800

    updated font and font size

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,25 @@
 import "./globals.css";
-import { Roboto_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import { Providers } from "./providers";
 
-export const roboto_mono = Roboto_Mono({
-  subsets: ["latin"],
-  variable: "--font-roboto-mono",
+export const robotoMono = localFont({
+  src: "./RobotoMono.ttf",
   display: "swap",
 });
 
 export const metadata = {
-  title: "Justin Sun",
-  description: "Justin Sun",
+  title: "Justin Sun 孙盈廷",
+  description: "Justin Sun 孙盈廷",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <body
-        className={`h-full bg-white dark:bg-black text-black dark:text-white text-xs sm:text-base ${roboto_mono.variable}`}
+        className={
+          "h-full bg-white dark:bg-black text-black dark:text-white text-base " +
+          robotoMono.className
+        }
       >
         <Providers>{children}</Providers>
       </body>
